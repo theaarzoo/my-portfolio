09ac5fedb7e7f65b68e1931ef83bb1fb532da6e2
Md. Aarzoo Alam <aarzooalam11@gmail.com>
2026-05-16 11:58:58 +0530

    new footer design

--- src/config/Footer.tsx
+++ src/config/Footer.tsx
@@ -1,5 +1,52 @@
+export interface FooterLink {
+  label: string;
+  href: string;
+}
+
 export const footerConfig = {
-  developer: 'theaarzoo',
+  developer: 'Md. Aarzoo Alam',
   text: 'Design & Developed by',
   copyright: 'All rights reserved.',
+  sections: {
+    nav: 'Navigate',
+    connect: 'Connect',
+  },
+  navItems: [
+    {
+      label: 'Home',
+      href: '/',
+    },
+    {
+      label: 'Work',
+      href: '/work-experience',
+    },
+    {
+      label: 'Blog',
+      href: '/blog',
+    },
+    {
+      label: 'Resume',
+      href: '/resume',
+    },
+    {
+      label: 'Projects',
+      href: '/projects',
+    },
+    {
+      label: 'Gears',
+      href: '/gears',
+    },
+    {
+      label: 'Setup',
+      href: '/setup',
+    },
+    {
+      label: 'Journey',
+      href: '/journey',
+    },
+    {
+      label: 'Contact',
+      href: '/contact',
+    },
+  ] as FooterLink[],
 };
